3d0b6e79bad97f5cb28e6cf1757a020514ccb459
morrigan <belma.dzi@gmail.com>
2019-12-17 14:53:51 +0100

    Replace npm commands with yarn

--- tasksfile.ts
+++ tasksfile.ts
@@ -7,15 +7,15 @@ function clean() {
 }
 
 function lint() {
-    sh("docker-compose run --rm backend npm run lint", {async: false, nopipe: true});
+    sh("docker-compose run --rm backend yarn run lint", {async: false, nopipe: true});
 }
 
 function npm(options: any, task = "--help") {
-    sh("docker-compose run --rm backend npm run " + task, {async: false, nopipe: true});
+    sh("docker-compose run --rm backend yarn run " + task, {async: false, nopipe: true});
 }
 
 function compile() {
-    sh("docker-compose run --rm backend npm run build", {async: false, nopipe: true});
+    sh("docker-compose run --rm backend yarn run build", {async: false, nopipe: true});
 }
 
 function build() {
@@ -25,11 +25,11 @@ function build() {
 }
 
 function e2e() {
-    sh("docker-compose run -e NODE_ENV=test --rm backend npm run test:e2e", {async: false, nopipe: true});
+    sh("docker-compose run -e NODE_ENV=test --rm backend yarn run test:e2e", {async: false, nopipe: true});
 }
 
 function unit() {
-    sh("docker-compose run -e NODE_ENV=test --no-deps --rm backend npm run test:unit", {async: false, nopipe: true});
+    sh("docker-compose run -e NODE_ENV=test --no-deps --rm backend yarn run test:unit", {async: false, nopipe: true});
 }
 
 function test(options: ICLIOptions, type: string = "") {
@@ -38,7 +38,7 @@ function test(options: ICLIOptions, type: string = "") {
     } else if (type === "e2e") {
         e2e();
     } else {
-        sh("docker-compose run -e NODE_ENV=test --no-deps --rm backend npm run test", {async: false, nopipe: true});
+        sh("docker-compose run -e NODE_ENV=test --no-deps --rm backend yarn run test", {async: false, nopipe: true});
     }
 }
 
@@ -50,7 +50,7 @@ help(e2e, "Runs end-to-end tests");
 help(unit, "Runs nodejs unit tests");
 help(test, "Runs nodejs tests");
 help(dev, "Starts application and all dependent services");
-help(npm, "Executes npm script");
+help(npm, "Executes yarn script");
 help(compile, "Transpiles files to es5");
 help(clean, "Removes all build directories and dependencies");
 help(lint, "Runs eslint on current project");
